caff13bb74d22cc39ff373d21f5d65af21f1ee60
dharashah <dharashah@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-17 15:56:43 +0000

    missed to upgrade version
    git-svn-id: https://plugins.svn.wordpress.org/st-daily-tip/trunk@626455 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- st-daily-tip.php
+++ st-daily-tip.php
@@ -7,12 +7,12 @@ if (function_exists('add_daily_tip')) {
 		print add_daily_tip('[stdailytip group="Tip"]');
 	}
 	?>
-Version: 0.9
+Version: 1.0
 Author: Dhara Shah
 Author URI: http://sanskrutitech.in/
 License: GPL
 */
-define('WP_DAILY_TIP_VERSION', "0.9");
+define('WP_DAILY_TIP_VERSION', "1.0");
 define('WP_DAILY_TIP_FOLDER', dirname(plugin_basename(__FILE__)));
 define('WP_DAILY_TIP_URL', plugins_url('',__FILE__));
 
@@ -42,7 +42,7 @@ register_deactivation_hook( __FILE__, 'st_daily_tip_uninstall' );
 global $st_daily_tip_db_ver;
 global $table_suffix;
 
-$st_daily_tip_db_ver = "0.9";
+$st_daily_tip_db_ver = "1.0";
 $table_suffix = "dailytipdata";
 
 function select_today_tip($group){
